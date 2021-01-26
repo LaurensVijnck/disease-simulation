cc969316a3438d61b63d4f09fa4fab5d4b1943de
signemoegelmose <55746040+signemoegelmose@users.noreply.github.com>
2021-01-26 10:58:56 +0100

    lognormal distribution for days until death

--- disease/transmission.py
+++ disease/transmission.py
@@ -29,16 +29,16 @@ class Transmission:
 
         # TODO: Allow supplying the following probabilities via config
         self.__beta_pop = {
-            DiseaseStateEnum.STATE_INFECTED: 1,
-            DiseaseStateEnum.STATE_SYMPTOMATIC: 1,
-            DiseaseStateEnum.STATE_ASYMPTOMATIC: 1,
+            DiseaseStateEnum.STATE_INFECTED: 5,
+            DiseaseStateEnum.STATE_SYMPTOMATIC: 5,
+            DiseaseStateEnum.STATE_ASYMPTOMATIC: 5,
         }
 
         # TODO: Allow supplying the following probabilities via config
         self.__beta_household = {
-            DiseaseStateEnum.STATE_INFECTED: 0.2,
-            DiseaseStateEnum.STATE_SYMPTOMATIC: 0.2,
-            DiseaseStateEnum.STATE_ASYMPTOMATIC: 0.2,
+            DiseaseStateEnum.STATE_INFECTED: 0.25,
+            DiseaseStateEnum.STATE_SYMPTOMATIC: 0.25,
+            DiseaseStateEnum.STATE_ASYMPTOMATIC: 0.25,
         }
 
         self.__pop_contact = self.__parse_simple_contact_matrix(config.get("pop_matrix", None))
